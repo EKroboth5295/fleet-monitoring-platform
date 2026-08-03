96d655255290155fe017d0f69207688b02192c74
Evan Kroboth <evankroboth1@gmail.com>
2026-08-03 18:49:59 -0400

    Expanding vehicle history for all 20 Trucks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,52 +20,60 @@ function App() {
   console.log("APP LOADED");
 
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
-  const [history, setHistory] = useState<any[]>([]);
+  const [histories, setHistories] = useState<Record<number, any[]>>({});
 
-  const pathCoordinates = history.map(point => [
+  const pathCoordinates = (histories[1] || []).map(point => [
   point.lat,
   point.lon
 ]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      fetch("http://127.0.0.1:8000/vehicles")
-        .then(response => response.json())
-        .then(data => {
-          setVehicles(data);
-        });
-      fetch("http://127.0.0.1:8000/history/1")
-        .then(response => response.json())
-        .then(historyData => {
-          console.log("History length:", historyData.length);
-          setHistory(historyData);
-        });
-    }, 3000);
+  const interval = setInterval(() => {
+  console.log(histories);
+  
+    fetch("http://127.0.0.1:8000/vehicles")
+      .then(response => response.json())
+      .then(data => {
 
-    return () => clearInterval(interval);
-  }, []);
+        setVehicles(data);
+
+        data.forEach((vehicle: Vehicle) => {
+
+          fetch(`http://127.0.0.1:8000/history/${vehicle.id}`)
+            .then(response => response.json())
+            .then(historyData => {
+
+              setHistories(prev => ({
+                ...prev,
+                [vehicle.id]: historyData
+              }));
+
+            });
+
+        });
+
+      });
+
+  }, 3000);
+
+  return () => clearInterval(interval);
+
+}, []);
 
   useEffect(() => {
 
     fetch("http://127.0.0.1:8000/vehicles")
-        .then(res => res.json())
-        .then(data => setVehicles(data));
+      .then(res => res.json())
+      .then(data => setVehicles(data));
 
-    fetch("http://127.0.0.1:8000/history/1")
-        .then(res => res.json())
-        .then(historyData => {
-            console.log(historyData);
-            setHistory(historyData);
-        });
-
-}, []);
+  }, []);
 
   return (
     <div>
       <h1>Fleet Dashboard</h1>
       <p>Real-time Vehicle Monitoring System</p>
 
-      <h2>History points: {history.length}</h2>
+      <h2>History points: {(histories[1] || []).length}</h2>
 
       <h2>Vehicles:</h2>
 
@@ -79,20 +87,34 @@ function App() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <Polyline positions={pathCoordinates} />
+        {vehicles.map((vehicle) => {
 
-        {vehicles.map((vehicle) => (
-          <Marker
-            key={vehicle.id}
-            position={[vehicle.lat, vehicle.lon]}
-          >
-            <Popup>
-              Truck {vehicle.id}
-              <br />
-              Speed: {vehicle.speed} mph
-            </Popup>
-          </Marker>
-        ))}
+          const pathCoordinates = (histories[vehicle.id] || []).map(point => [
+            point.lat,
+            point.lon
+          ]);
+
+          return (
+            <>
+              <Polyline
+                key={`line-${vehicle.id}`}
+                positions={pathCoordinates}
+              />
+
+              <Marker
+                key={`marker-${vehicle.id}`}
+                position={[vehicle.lat, vehicle.lon]}
+              >
+                <Popup>
+                  Truck {vehicle.id}
+                  <br />
+                  Speed: {vehicle.speed} mph
+                </Popup>
+              </Marker>
+            </>
+          );
+
+        })}
 
       </MapContainer>
 
